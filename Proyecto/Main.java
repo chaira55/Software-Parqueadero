248725d62911d07fd2196ddfdf197d41bf5a143b
chaira55 <sharik.guzmanar@amigo.edu.co>
2023-10-19 09:01:30 -0500

    Crear los servicios y el main para poder ejecutar los metodos de los servicios

--- Proyecto/Main.java
+++ Proyecto/Main.java
@@ -1,6 +1,127 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        Servicio servicio = new Servicio();
+
+        int opcion;
+        do {
+            System.out.println("=== Menú de Servicio ===");
+            System.out.println("1. Registrar Servicio de Lavado de Auto");
+            System.out.println("2. Crear Punto de Carga para Vehículos Eléctricos");
+            System.out.println("3. Actualizar Punto de Carga para Vehículos Eléctricos");
+            System.out.println("4. Eliminar Punto de Carga para Vehículos Eléctricos");
+            System.out.println("5. Consultar Puntos de Carga para Vehículos Eléctricos Disponibles");
+            System.out.println("6. Asignar Punto de Carga para Vehículos Eléctricos");
+            System.out.println("7. Crear Servicio Adicional");
+            System.out.println("8. Actualizar Servicio Adicional");
+            System.out.println("9. Eliminar Servicio Adicional");
+            System.out.println("10. Consultar Saldo del Cliente");
+            System.out.println("11. Registrar Carga de Auto");
+            System.out.println("12. Cancelar Servicio");
+            System.out.println("0. Salir");
+
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Consumir la nueva línea después del número
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Nombre del cliente: ");
+                    String cliente = scanner.nextLine();
+                    System.out.print("Tipo de Lavado (1: Básico, 2: Completo): ");
+                    int tipoLavado = scanner.nextInt();
+                    servicio.registrarServicioLavadoAuto(cliente, tipoLavado, new Date());
+                    break;
+                case 2:
+                    System.out.print("Ubicación del Punto de Carga: ");
+                    String ubicacion = scanner.nextLine();
+                    System.out.print("Capacidad del Punto de Carga: ");
+                    int capacidad = scanner.nextInt();
+                    servicio.crearPuntoCargaEV(ubicacion, capacidad);
+                    break;
+                case 3:
+                    System.out.print("Ubicación del Punto de Carga a actualizar: ");
+                    ubicacion = scanner.nextLine();
+                    System.out.print("¿Está ocupado? (true o false): ");
+                    boolean ocupado = scanner.nextBoolean();
+                    servicio.actualizarPuntoCargaEV(ubicacion, ocupado);
+                    break;
+                case 4:
+                    System.out.print("Ubicación del Punto de Carga a eliminar: ");
+                    ubicacion = scanner.nextLine();
+                    servicio.eliminarPuntoCargaEV(ubicacion);
+                    break;
+                case 5:
+                    ArrayList<PuntoCargaEV> puntosDisponibles = servicio.consultarPuntosCargaEVDisponibles();
+                    System.out.println("Puntos de Carga para Vehículos Eléctricos Disponibles:");
+                    for (PuntoCargaEV punto : puntosDisponibles) {
+                        System.out.println("Ubicación: " + punto.getUbicacion() + ", Capacidad: " + punto.getCapacidad());
+                    }
+                    break;
+                case 6:
+                    System.out.print("Ubicación del Punto de Carga a asignar: ");
+                    ubicacion = scanner.nextLine();
+                    boolean asignado = servicio.asignarPuntoCargaEV(ubicacion);
+                    if (asignado) {
+                        System.out.println("Punto de Carga asignado exitosamente.");
+                    } else {
+                        System.out.println("No se pudo asignar el Punto de Carga.");
+                    }
+                    break;
+                case 7:
+                    System.out.print("Nombre del Servicio Adicional: ");
+                    String nombreServicio = scanner.nextLine();
+                    System.out.println("Opciones para el Servicio Adicional:");
+                    System.out.println("1. Encerar Auto");
+                    System.out.println("2. Descontaminación");
+                    System.out.println("3. Aromatización");
+                    System.out.println("4. Cambio de Aceite");
+                    System.out.print("Seleccione una opción: ");
+                    int opcionServicioAdicional = scanner.nextInt();
+                    servicio.crearServicioAdicional(nombreServicio, opcionServicioAdicional);
+                    break;
+                case 8:
+                    System.out.print("ID del Servicio Adicional a actualizar: ");
+                    int idServicioAdicional = scanner.nextInt();
+                    System.out.print("Nueva tarifa de uso: ");
+                    float nuevaTarifaUso = scanner.nextFloat();
+                    servicio.actualizarServicioAdicional(idServicioAdicional, nuevaTarifaUso);
+                    break;
+                case 9:
+                    System.out.print("ID del Servicio Adicional a eliminar: ");
+                    idServicioAdicional = scanner.nextInt();
+                    servicio.eliminarServicioAdicional(idServicioAdicional);
+                    break;
+                case 10:
+                    System.out.print("Nombre del cliente para consultar el saldo: ");
+                    String nombreCliente = scanner.nextLine();
+                    servicio.obtenerSaldoCliente(nombreCliente);
+                    break;
+                case 11:
+                    System.out.print("Nombre del cliente para registrar la carga de auto: ");
+                    cliente = scanner.nextLine();
+                    System.out.print("Ubicación para la carga de auto: ");
+                    String ubicacionCarga = scanner.nextLine();
+                    servicio.registrarCargaAuto(cliente, ubicacionCarga);
+                    break;
+                case 12:
+                    System.out.print("Nombre del cliente para cancelar el servicio: ");
+                    cliente = scanner.nextLine();
+                    System.out.print("Descripción del servicio a cancelar: ");
+                    String descripcion = scanner.nextLine();
+                    servicio.cancelarServicio(cliente, descripcion);
+                    break;
+                case 0:
+                    System.out.println("Saliendo del programa.");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+                    break;
+            }
+        } while (opcion != 0);
     }
-    
 }
--- Proyecto/Servicio.java
+++ Proyecto/Servicio.java
@@ -1,20 +1,280 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Servicio {
     private int id;
-    private String nombre;
-    private String descripcion;
+    private String cliente;
     private float tarifaUso;
+    private float saldoTotal;
+    private ArrayList<String> serviciosRegistrados;
+    private ArrayList<PuntoCargaEV> puntosCargaEV;
+    private ArrayList<ServicioAdicional> serviciosAdicionales;
 
     public Servicio() {
         this.id = 0;
-        this.nombre = "";
-        this.descripcion = "";
+        this.cliente = "";
         this.tarifaUso = 0.0f;
+        this.saldoTotal = 0.0f;
+        this.serviciosRegistrados = new ArrayList<>();
+        this.puntosCargaEV = new ArrayList<>();
+        this.serviciosAdicionales = new ArrayList<>();
+        
     }
 
-    public Servicio(int id, String nombre, String descripcion, float tarifaUso){
+
+    public String getCliente() {
+        return cliente;
+    }
+    
+    public float getSaldoTotal() {
+        return saldoTotal;
+    }
+    
+
+    public void registrarServicioLavadoAuto(String cliente, int tipoLavado, Date fecha) {    
+        if (tipoLavado == 1) { // lavado basico
+            this.tarifaUso = 25000.0f;
+        } else if (tipoLavado == 2) { // lavado completo
+            this.tarifaUso = 78000.0f;
+        }
+
+        if (this.tarifaUso > 0.0f) {
+            this.saldoTotal += this.tarifaUso;
+            String servicioRegistrado = "Cliente: " + cliente + ", Tipo de Lavado: " + tipoLavado + ", Fecha: " + fecha.toString() + ", Costo: $" + this.tarifaUso;
+            serviciosRegistrados.add(servicioRegistrado);
+        } else {
+            System.out.println("Tipo de lavado no válido: " + tipoLavado);
+        }
+    }
+     
+
+     public void crearPuntoCargaEV(String ubicacion, int capacidad) {
+        PuntoCargaEV nuevoPuntoCarga = new PuntoCargaEV(ubicacion, capacidad);
+        puntosCargaEV.add(nuevoPuntoCarga);
+    }
+
+    public void actualizarPuntoCargaEV(String ubicacion, boolean ocupado) {
+        for (PuntoCargaEV puntoCarga : puntosCargaEV) {
+            if (puntoCarga.getUbicacion().equals(ubicacion)) {
+                puntoCarga.setOcupado(ocupado);
+                return;
+            }
+        }
+    }
+
+    public void eliminarPuntoCargaEV(String ubicacion) {
+        puntosCargaEV.removeIf(puntoCarga -> puntoCarga.getUbicacion().equals(ubicacion));
+    }
+
+    public ArrayList<PuntoCargaEV> consultarPuntosCargaEVDisponibles() {
+        ArrayList<PuntoCargaEV> puntosDisponibles = new ArrayList<>();
+        for (PuntoCargaEV puntoCarga : puntosCargaEV) {
+            if (!puntoCarga.isOcupado()) {
+                puntosDisponibles.add(puntoCarga);
+            }
+        }
+        return puntosDisponibles;
+    }
+    
+
+    public boolean asignarPuntoCargaEV(String ubicacion) {
+        for (PuntoCargaEV puntoCarga : puntosCargaEV) {
+            if (puntoCarga.getUbicacion().equals(ubicacion) && !puntoCarga.isOcupado()) {
+                puntoCarga.setOcupado(true);
+                this.tarifaUso = 125000.0f;
+                return true;
+            }
+        }
+        return false; 
+    }
+
+    public void crearServicioAdicional(String nombre, int op) {
+        ServicioAdicional nuevoServicioAdicional;
+         for (ServicioAdicional sa : serviciosAdicionales) {
+            if (sa.getNombre() == nombre) {
+                sa.setNombre(nombre);
+    
+                switch (op) {
+                    case 1: // encerar auto
+                        sa.setTarifaUso(50000.0f);
+                        nuevoServicioAdicional = new ServicioAdicional(id, nombre, sa.getTarifaUso());
+                        serviciosAdicionales.add(nuevoServicioAdicional);
+                        break;
+                    case 2: // descontaminacion
+                        sa.setTarifaUso(35000.0f);
+                        nuevoServicioAdicional = new ServicioAdicional(id, nombre, sa.getTarifaUso());
+                        serviciosAdicionales.add(nuevoServicioAdicional);
+                        break;
+                    case 3: // aromatizacion
+                        sa.setTarifaUso(28000.0f);
+                        nuevoServicioAdicional = new ServicioAdicional(id, nombre, sa.getTarifaUso());
+                        serviciosAdicionales.add(nuevoServicioAdicional);
+                        break;
+                    case 4: // cambio de aceite
+                        sa.setTarifaUso(76000.0f);
+                        nuevoServicioAdicional = new ServicioAdicional(id, nombre, sa.getTarifaUso());
+                        serviciosAdicionales.add(nuevoServicioAdicional);
+                        break;
+                    default:
+                        System.out.println("Opción no válida para actualizar la tarifa de uso.");
+                        return;
+                    
+                }
+            }
+        }
+
+    if (tarifaUso > 0.0f) {
+        this.saldoTotal += this.tarifaUso;
+        System.out.println("Servicio Adicional:\n ID " + id + ", Nombre " + nombre + ", Costo: $" + tarifaUso);
+    } else {
+        System.out.println("No se pudo crear el Servicio Adicional debido a una tarifa inválida.");
+    }
+
+    }
+
+    public void actualizarServicioAdicional(int id, float tarifaUso) {
+        for (ServicioAdicional sa : serviciosAdicionales) {
+            if (sa.getId() == id) {
+                sa.setTarifaUso(tarifaUso);
+            }
+        }
+    }
+    
+
+    public void eliminarServicioAdicional(int id) {
+        serviciosAdicionales.removeIf(sa -> sa.getId() == id);
+    }
+
+    public ServicioAdicional consultarServicioAdicional(int id) {
+        for (ServicioAdicional sa : serviciosAdicionales) {
+            if (sa.getId() == id) {
+                return sa;
+            }
+        }
+        return null;
+    }
+
+
+    public void registrarServicioAdicional(String cliente, int idServicioAdicional) {
+        ServicioAdicional servicioAdicional = consultarServicioAdicional(idServicioAdicional);
+        if (servicioAdicional != null) {
+            this.saldoTotal += servicioAdicional.getTarifaUso();
+            String servicioRegistrado = "Cliente: " + cliente + ", Servicio Adicional: " + servicioAdicional.getNombre() + ", Costo: $" + servicioAdicional.getTarifaUso();
+            serviciosRegistrados.add(servicioRegistrado);
+        } else {
+            System.out.println("Servicio adicional no válido: " + idServicioAdicional);
+        }
+    }
+
+    public void registrarCargaAuto(String cliente, String ubicacion) {
+        PuntoCargaEV puntoCarga = buscarPuntoCargaEV(ubicacion);
+        if (puntoCarga != null && !puntoCarga.isOcupado()) {
+            this.tarifaUso = 125000.0f;
+            puntoCarga.setOcupado(true);
+            this.saldoTotal += this.tarifaUso;
+            String servicioRegistrado = "Cliente: " + cliente + ", Carga de Auto en: " + ubicacion + ", Costo: $" + this.tarifaUso;
+            serviciosRegistrados.add(servicioRegistrado);
+        } else {
+            System.out.println("No se pudo registrar la carga de auto en: " + ubicacion);
+        }
+    }
+
+    public void cancelarServicio(String cliente, String descripcion) {
+        RegistroServicio servicioAEliminar = null;
+        for (RegistroServicio servicio : serviciosRegistrados) {
+            if (servicio.getCliente().equals(cliente) && servicio.getDescripcion().equals(descripcion)) {
+                servicioAEliminar = servicio;
+                break;
+            }
+        }
+        
+        if (servicioAEliminar != null) {
+            serviciosRegistrados.remove(servicioAEliminar);
+            this.saldoTotal -= servicioAEliminar.getCosto();
+            System.out.println("Servicio cancelado: " + descripcion);
+        } else {
+            System.out.println("El servicio no se encontró en los registros: " + descripcion);
+        }
+    }
+    
+    
+    public void obtenerSaldoCliente(String nombreCliente) {
+        float saldoCliente = 0.0f;
+        for (String servicio : serviciosRegistrados) {
+            if (servicio.startsWith("Cliente: " + nombreCliente)) {
+                // Analiza la descripción para obtener el costo del servicio
+                String[] partes = servicio.split(", Costo: \\$");
+                if (partes.length == 2) {
+                    try {
+                        saldoCliente += Float.parseFloat(partes[1]);
+                    } catch (NumberFormatException e) {
+                        // Manejo de error en caso de que no se pueda analizar el costo
+                    }
+                }
+            }
+        }
+    
+        if (saldoCliente > 0.0f) {
+            System.out.println("Cliente: " + nombreCliente);
+            System.out.println("Saldo Total: $" + saldoCliente);
+        } else {
+            System.out.println("Cliente no encontrado o saldo total no disponible.");
+        }
+    }
+    
+    private PuntoCargaEV buscarPuntoCargaEV(String ubicacion) {
+    for (PuntoCargaEV puntoCarga : puntosCargaEV) {
+        if (puntoCarga.getUbicacion().equals(ubicacion)) {
+            return puntoCarga;
+        }
+    }
+    return null;
+}
+
+    
+}
+
+
+class PuntoCargaEV {
+    private String ubicacion;
+    private int capacidad;
+    private boolean ocupado;
+
+    public PuntoCargaEV(String ubicacion, int capacidad) {
+        this.ubicacion = ubicacion;
+        this.capacidad = capacidad;
+        this.ocupado = false;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+}
+
+
+class ServicioAdicional {
+    private int id;
+    private String nombre;
+    private float tarifaUso;
+    public Object mostrarSaldo;
+
+    private static ArrayList<ServicioAdicional> serviciosAdicionales = new ArrayList<>();
+
+    public ServicioAdicional(int id, String nombre, float tarifaUso) {
         this.id = id;
         this.nombre = nombre;
-        this.descripcion = descripcion;
         this.tarifaUso = tarifaUso;
     }
 
@@ -34,14 +294,6 @@ public class Servicio {
         this.nombre = nombre;
     }
 
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
     public float getTarifaUso() {
         return tarifaUso;
     }
@@ -49,7 +301,28 @@ public class Servicio {
     public void setTarifaUso(float tarifaUso) {
         this.tarifaUso = tarifaUso;
     }
+}
 
-    
+class RegistroServicio {
+    private String cliente;
+    private String descripcion;
+    private float costo;
 
+    public RegistroServicio(String cliente, String descripcion, float costo) {
+        this.cliente = cliente;
+        this.descripcion = descripcion;
+        this.costo = costo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public float getCosto() {
+        return costo;
+    }
 }
